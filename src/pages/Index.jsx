f11e754829a0733dcc615816734d36ac576eed32
MiguelRequenaR <e_2019100844l@uncp.edu.pe>
2023-09-28 17:28:36 -0500

    add form

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,4 +1,5 @@
 import { useLoaderData } from "react-router-dom";
+import Cliente from "../components/Cliente";
 
 export function loader (){
       const clientes = [
@@ -50,6 +51,28 @@ function Index () {
     <>
       <h1 className='font-black text-4xl text-blue-900'>Clientes</h1>
       <p className='mt-3'>Administra tus clientes</p>
+
+      {datos.length ? (
+        <table className="w-full bg-white shadow mt-5 table-auto">
+          <thead className="bg-blue-800 text-white">
+            <tr>
+              <th className="p-2">Clientes</th>
+              <th className="p-2">Contacto</th>
+              <th className="p-2">Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {datos.map(cliente => (
+              <Cliente
+                cliente={cliente}
+                key={cliente.id}
+              />
+            ))}
+          </tbody>
+        </table>
+      ):(
+        <p className='mt-10 text-center text-2xl'>No hay clientes, comienza creando uno</p>
+      )}
     </>
   )
 }
--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,10 +1,35 @@
+import { useNavigate } from "react-router-dom"
+import Formulario from "../components/Formulario";
 
+function NuevoCliente() {
 
-const NuevoCliente = () => {
+  const navigate = useNavigate();
+
   return (
-    <div>
-        <h1>Nuevo Cliente</h1>
-    </div>
+    <>
+      <h1 className="font-black text-4xl text-blue-900">Nuevo Cliente</h1>
+      <p className="mt-3">Llena todos los campos para registrar un nuevo cliente</p>
+
+      <div className="flex justify-end">
+        <button
+          className="bg-blue-800 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded"
+          onClick={() => navigate('/')}
+        >
+          Volver a Clientes
+        </button>
+      </div>
+
+      <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-20">
+        <form action="">
+          <Formulario/>
+          <input 
+            type="submit"
+            className="bg-indigo-600 hover:bg-indigo-700 w-full mt-5 p-2 text-white uppercase font-bold" 
+            value="Registrar Cliente"
+          />
+        </form>
+      </div>
+    </>
   )
 }
 
